358c09d83450e9365f59ba430b30363f82eb4e23
Collins Ikiara <>
2026-09-03 09:10:20 +0300

    Create GET and POST HTTP methods.
    Added HTTP methods for a cats object from class Cat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service.js';
 
-@Controller()
+@Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  // GET /animals/cats
+  @Get('')
+  getCats(): Cat[] {
+    return cats;
   }
+
+  // POST /animals/cats
+  @Post('')
+  createCat(@Req() req: any): Cat[] {
+    const cat = new Cat(req.body.name, req.body.age);
+    cats.push(cat);
+    return cats;
+  }
+}
+let cats: Cat[] = [];
+class Cat {
+  constructor(private name: string, private age: number) {}
 }
