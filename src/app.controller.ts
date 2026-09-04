2b260e6604d60c6204b53c461bc281085287491e
Collins Ikiara <>
2026-09-04 09:15:01 +0300

    Add Request type from Express to be used for the POST handler. Also moved the class Cat and cats object above the Controller so as to initialize the Cat type before use.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,25 +1,27 @@
-import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req } from '@nestjs/common';
 import { AppService } from './app.service.js';
+import type { Request } from 'express';
+
+let cats: Cat[] = [];
+class Cat {
+  constructor(private name: string, private age: number) {}
+}
 
 @Controller('cats')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   // GET /animals/cats
-  @Get('')
+  @Get()
   getCats(): Cat[] {
     return cats;
   }
 
   // POST /animals/cats
   @Post('')
-  createCat(@Req() req: any): Cat[] {
+  createCat(@Req() req: Request): Cat {
     const cat = new Cat(req.body.name, req.body.age);
     cats.push(cat);
-    return cats;
+    return cat;
   }
-}
-let cats: Cat[] = [];
-class Cat {
-  constructor(private name: string, private age: number) {}
 }
